92c745bde75aec6944af4798e650c6a5c29a61bc
hippoz <hippozheng@163.com>
2018-10-16 13:56:33 +0800

    [LAB5] Subscribe to a Kitchen Domain Event from within an Aggregate and Create CQRS View

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedKitchenOrderRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedKitchenOrderRepository.java
@@ -1,5 +1,6 @@
 package com.mattstine.dddworkshop.pizzashop.kitchen;
 
+import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Event;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.EventLog;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.adapters.InProcessEventSourcedRepository;
@@ -10,6 +11,8 @@ import java.util.Map;
 
 final class InProcessEventSourcedKitchenOrderRepository extends InProcessEventSourcedRepository<KitchenOrderRef, KitchenOrder, KitchenOrder.OrderState, KitchenOrderEvent, KitchenOrderAddedEvent> implements KitchenOrderRepository {
 
+    Map<OnlineOrderRef, KitchenOrderRef> orderRefKitchenOrderRefMap = new HashMap<>();
+
     InProcessEventSourcedKitchenOrderRepository(EventLog eventLog, Topic topic) {
         super(eventLog,
                 KitchenOrderRef.class,
@@ -17,10 +20,23 @@ final class InProcessEventSourcedKitchenOrderRepository extends InProcessEventSo
                 KitchenOrder.OrderState.class,
                 KitchenOrderAddedEvent.class,
                 topic);
+        eventLog.subscribe(topic, this::handleEvent);
+    }
+
+    private void handleEvent(Event evt) {
+        if (evt instanceof KitchenOrderAddedEvent) {
+            KitchenOrderAddedEvent koae = (KitchenOrderAddedEvent) evt;
+            orderRefKitchenOrderRefMap.put(koae.getState().getOnlineOrderRef(),
+                    koae.getRef());
+        }
     }
 
     @Override
     public KitchenOrder findByOnlineOrderRef(OnlineOrderRef onlineOrderRef) {
+        KitchenOrderRef ref = orderRefKitchenOrderRefMap.get(onlineOrderRef);
+        if (ref != null) {
+            return findByRef(ref);
+        }
         return null;
     }
 }
--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
@@ -1,9 +1,11 @@
 package com.mattstine.dddworkshop.pizzashop.kitchen;
 
+import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Event;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.EventLog;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.adapters.InProcessEventSourcedRepository;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,12 +14,29 @@ import java.util.stream.Collectors;
 
 final class InProcessEventSourcedPizzaRepository extends InProcessEventSourcedRepository<PizzaRef, Pizza, Pizza.PizzaState, PizzaEvent, PizzaAddedEvent> implements PizzaRepository {
 
+    final Map<KitchenOrderRef, Set<PizzaRef>> kitchenOrderRefSetMap = new HashMap<>();
+
     InProcessEventSourcedPizzaRepository(EventLog eventLog, Topic pizzas) {
         super(eventLog, PizzaRef.class, Pizza.class, Pizza.PizzaState.class, PizzaAddedEvent.class, pizzas);
+        eventLog.subscribe(pizzas, this::handleEvent);
+    }
+
+    private void handleEvent(Event evt) {
+        if (evt instanceof PizzaAddedEvent) {
+            PizzaAddedEvent pae = (PizzaAddedEvent) evt;
+            Set<PizzaRef> refs = kitchenOrderRefSetMap.
+                    computeIfAbsent(pae.getState().getKitchenOrderRef(),
+                            r -> new HashSet<>());
+            refs.add(pae.getRef());
+        }
     }
 
     @Override
     public Set<Pizza> findPizzasByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
-        return null;
+        Set<PizzaRef> refs = kitchenOrderRefSetMap.get(kitchenOrderRef);
+        if (refs != null) {
+            return refs.stream().map(this::findByRef).collect(Collectors.toSet());
+        }
+        return Collections.emptySet();
     }
 }
